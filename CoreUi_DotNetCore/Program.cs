7feea70edc0aef692ffa831a853813dfc0fc521a
HMARKO <hozjan.marko@windowslive.com>
2018-03-16 07:11:38 +0100

    Add project files.

---
+++ CoreUi_DotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreUi_DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
